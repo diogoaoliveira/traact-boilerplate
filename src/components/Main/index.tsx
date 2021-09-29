c661548d142829e837a8efd8cd5f098672eda9f8
Diogo Augusto <diogo.augustoo@gmail.com>
2021-09-28 23:13:52 -0300

    first commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+const Main = ({
+  title = 'Next.js + TS Boilerplate',
+  description = 'TypeScript, ReactJS, NextJS and Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="JS Developer" />
+  </S.Wrapper>
+)
+
+export default Main
